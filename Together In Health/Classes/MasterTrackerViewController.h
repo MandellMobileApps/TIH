b2452f5548fc14a6b72be6461e48c81373dafb45
Jon Mandell <jonmandell@macbook-pro-4.lan>
2017-10-03 17:20:59 -0700

    Created Master Tracker

---
+++ Together In Health/Classes/MasterTrackerViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterTrackerViewController.h
+//  Together In Health
+//
+//  Created by Jon Mandell on 10/3/17.
+//  Copyright © 2017 MandellMobileApps. All rights reserved.
+//
+
+#import "AbstractViewController.h"
+
+@interface MasterTrackerViewController : AbstractViewController
+
+@end

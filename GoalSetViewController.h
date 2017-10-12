1e39ec9c555c883c596f5aca3c111e669eed12eb
Cami Mandell <Cami@107-128-97-217.lightspeed.sntcca.sbcglobal.net>
2017-10-12 10:46:37 -0700

    Changes to goal controller try 2

--- GoalSetViewController.h
+++ GoalSetViewController.h
@@ -7,7 +7,30 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AbstractViewController.h"
+#import "Goal1ViewController.h"
+#import "Goal2ViewController.h"
+#import "Goal3ViewController.h"
 
-@interface GoalSetViewController : UICollectionView
+@interface GoalSetViewController : AbstractViewController
+
+@property (nonatomic, strong)Goal1ViewController* goal1ViewController;
+@property (nonatomic, strong)Goal2ViewController* goal2ViewController;
+@property (nonatomic, strong)Goal3ViewController* goal3ViewController;
+
+
+@property (nonatomic, strong) IBOutlet UIButton* goal1Button;
+@property (nonatomic, strong) IBOutlet UIButton* goal2Button;
+@property (nonatomic, strong) IBOutlet UIButton* goal3Button;
+
+
+@property (nonatomic) NSUInteger goalIndex;
+
+
+// Food Sub Contollers
+-(IBAction)goal1:(id)sender;
+-(IBAction)goal2:(id)sender;
+-(IBAction)goal3:(id)sender;
 
 @end
+

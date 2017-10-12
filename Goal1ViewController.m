094ea6e00bdceeb6cb5ab75416d491065ce4c16b
Cami Mandell <Cami@Camis-MacBook-Pro.local>
2017-10-12 09:08:59 -0700

    Set up goal controllers

---
+++ Goal1ViewController.m
@@ -0,0 +1,40 @@
+//
+//  Goal1ViewController.m
+//  Together In Health
+//
+//  Created by Cami Mandell on 10/11/17.
+//  Copyright © 2017 MandellMobileApps. All rights reserved.
+//
+
+#import "Goal1ViewController.h"
+#import "GoalSetViewController.h"
+
+@class GoalSetViewController;
+
+@interface Goal1ViewController ()
+
+@end
+
+@implementation Goal1ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
